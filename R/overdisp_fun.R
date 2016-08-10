7a6acef73fc4bdec0fde6e6d2feaed5446f4c3a4
Markus Huff <markus.huff@gmail.com>
2016-08-10 16:40:51 +0200

    New overdispersion check function

---
+++ R/overdisp_fun.R
@@ -0,0 +1,26 @@
+#' Test for overdispersion of glmer models (variance larger than the mean)
+#'
+#' @param model glmer model (fitted with the glmer() function of the lme4 package)
+#' @return Overdispersion measure.
+#' @description Overdispersion of a glmer model is given if the deviation is larger than the mean. If this is the case 
+#' (the ratio "prat" is larger than 1), it is recommended to calculate a variable with a unique value for each observation 
+#' (dat$obs_effect <- 1:nrow(dat)) and to include this variable as additional random term (+ (1|obs_effect)).
+#' @references http://glmm.wikidot.com/faq
+#' @references http://stats.stackexchange.com/questions/6989/how-do-i-fit-a-multilevel-model-for-over-dispersed-poisson-outcomes/9670#9670
+#' @references http://stats.stackexchange.com/questions/83611/how-to-fix-an-overdispersion-in-a-poisson-glmm-with-lmer-function-in-r
+#' @export
+#'
+overdisp_fun <- function(model) {
+  ## number of variance parameters in 
+  ##   an n-by-n variance-covariance matrix
+  vpars <- function(m) {
+    nrow(m)*(nrow(m)+1)/2
+  }
+  model.df <- sum(sapply(VarCorr(model),vpars))+length(fixef(model))
+  rdf <- nrow(model.frame(model))-model.df
+  rp <- residuals(model,type="pearson")
+  Pearson.chisq <- sum(rp^2)
+  prat <- Pearson.chisq/rdf
+  pval <- pchisq(Pearson.chisq, df=rdf, lower.tail=FALSE)
+  c(chisq=Pearson.chisq,ratio=prat,rdf=rdf,p=pval)
+}
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This package will (hopefully) grow constantly.
 
 Binaries are available:
 
+- [Version 0.3.3](https://www.dropbox.com/s/4hv1c6n6h5x55i3/PsychHelperFunctions_0.3.3.tgz?dl=0)
 - [Version 0.3.2](https://www.dropbox.com/s/w8n1xmbki7t8uym/PsychHelperFunctions_0.3.2.tgz?dl=0)
 - [Version 0.3.1](https://www.dropbox.com/s/72p029i2c2swakr/PsychHelperFunctions_0.3.1.tgz?dl=0)
 - [Version 0.3](https://www.dropbox.com/s/wu2hh7q4xer4175/PsychHelperFunctions_0.3.tgz?dl=0)
