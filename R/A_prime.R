65de2a951f7636242d2c240622b3cec5eae94fb1
markushuff <markus.huff@gmail.com>
2016-10-05 14:16:51 +0200

    Solved a problem with vectorized data

--- R/A_prime.R
+++ R/A_prime.R
@@ -10,9 +10,22 @@
 #'
 A_prime <- function(hit,fa)
 {
-  ifelse((hit >= 1 | fa <= 0),
-         stop(paste("A' is only defined for hits < 1 and fa > 0, the current values are hit =", hit,"fa =", fa)),
-         0.5 + (sign(hit - fa)) * ((hit - fa) ^ 2 + abs(hit - fa)) / 
-         (4 * max(hit, fa) - 4 * hit * fa))
+  if(any(hit >= 1) | any(fa <= 0))
+  {
+    stop(paste("A' is only defined for hits < 1 and fa > 0, the current values are hit =", hit,"fa =", fa))
+  }
+  else
+  {
+   A_prime <- 0.5 + (sign(hit - fa)) * ((hit - fa) ^ 2 + abs(hit - fa)) / 
+              (4 * max(hit, fa) - 4 * hit * fa)
+  }
+  return(A_prime)
 }
 
+# A_prime <- function(hit,fa)
+# {
+#   ifelse((hit >= 1 | fa <= 0),
+#          stop(paste("A' is only defined for hits < 1 and fa > 0, the current values are hit =", hit,"fa =", fa)),
+#          0.5 + (sign(hit - fa)) * ((hit - fa) ^ 2 + abs(hit - fa)) / 
+#            (4 * max(hit, fa) - 4 * hit * fa))
+# }
