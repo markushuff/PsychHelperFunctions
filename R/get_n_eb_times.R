f23626860694bc92eab2779e372d9de9ceaa90bf
markushuff <markus.huff@gmail.com>
2021-12-05 18:21:02 +0100

    some minor changes to get_n_et_times.R

--- R/get_n_eb_times.R
+++ R/get_n_eb_times.R
@@ -1,9 +1,9 @@
 #' n most salient event boundaries
 #'
-#'
-#' @param n number of the most salient event boundaries to return
+#' @author Markus Huff
+#' @param n umber of the most salient event boundaries in descending order to return
 #' @param segmag segmag object
-#' @return dataframe including the n most salient event boundaries
+#' @return dataframe including the n most salient event boundaries in descending order
 #' @description Get a list including the n highest event boundaries of a segmag object (Papenmeier, 2016; https://cran.r-project.org/package=segmag). Sometimes it is necessary to get just a list of the n highest event boundaries w/o doing the time consuming bootstrapping method. Note, this function requires the segmag package.
 #' @references Papenmeier, F., & Sering, K. (2014). segmag: Determine event boundaries in event segmentation experiments. R package version, 1(2).
 #' @export
